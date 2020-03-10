419403a2959a25562f6526aacc4ef6813904df12
Kate Poppy <k.makouskaya@gmail.com>
2020-03-10 16:03:06 +0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kate on 3/10/20.
+//  Copyright © 2020 k_poppy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
